06dd438449d03d2fe4f13ac9aded212181181379
bpereira74 <67373077+bpereira74@users.noreply.github.com>
2023-06-17 03:43:33 +0000

    Requerimiento 1: Creación de subclases con sus métodos

--- main.rb
+++ main.rb
@@ -8,24 +8,36 @@ end
 def birthday
 @age += 1
 end
-def talk
-if @type == "Student"
-puts "Aquí es la clase de programación con Ruby?"
-elsif @type == "Teacher"
-puts "Bienvenidos a la clase de programación con Ruby!"
-elsif @type == "Parent"
-puts "Aquí es la reunión de apoderados?"
+
+#R.1 Subclase Student, hereda de Clase Person  
+#R.1 contiene los métodos correspondientes, talk e introduce 
+class Student<Person
+      def talk
+            puts "Aquí es la clase de programación con Ruby?"
+      end
+      def introduce
+            puts "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
+      end
 end
+#Subclase Teacher, hereda de Clase Person
+#R.1 contiene los métodos correspondientes, talk e introduce  
+class Teacher<Person
+def talk
+      puts "Bienvenidos a la clase de programación con Ruby!"
 end
 def introduce
-if @type == "Student"
-puts "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
-elsif @type == "Teacher"
-puts "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
-elsif @type == "Parent"
-puts "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name}
-#{@last_name}."
+      puts "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
 end
 end
-end
+
+#Subclase Parent, hereda de Clase Person
+#R.1 contiene los métodos correspondientes, talk e introduce
+class Parent<Person
+      def talk
+            puts "Aquí es la reunión de apoderados?"
+      end
+      def introduce 
+      puts "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name}"
+      end
+end 
 
